018d05e7ea16504b53ab97b138caa7119f168066
Bogdan Bańkowski <Hevixxx@gmail.com>
2019-11-06 11:45:13 +0100

    moto class but now not empty xd

--- src/Codecool.CarRace/Moto.cs
+++ src/Codecool.CarRace/Moto.cs
@@ -4,7 +4,17 @@ using System.Text;
 
 namespace Codecool.CarRace
 {
-    class Moto
+    class Moto : Vehicle
     {
+        public static int MotorcycleNumber;
+        Moto(string name)
+        {
+            DistanceTraveled = 0;
+            MotorcycleNumber += 1;
+            Name = name;
+        }
+
+
+
     }
 }
