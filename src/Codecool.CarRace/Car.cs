19022a431e9eeda9e50a5a669bfe3aa88de9ae32
Joanna Truszczyńska <joanna.truszczynska@gmail.com>
2019-11-06 13:06:11 +0100

    PrepareForLap for cars

--- src/Codecool.CarRace/Car.cs
+++ src/Codecool.CarRace/Car.cs
@@ -5,12 +5,24 @@ namespace Codecool.CarRace
     public class Car : Vehicle
     {
         public int NormalSpeed { get; set; }
+        public int PrepareForLap()
+        {            
+            {
+                int speed;
+                if (Util.ChanceCalculator(5) is true)
+                    speed = 75;
+                else
+                    speed = Util.RandomNumber(80, 110);
+                return speed;
+            }
+        }
 
         public Car(string name)
         {
             Name = name;
             DistanceTraveled = 0;
             NormalSpeed = Util.RandomNumber(80, 110);
+           
         }
     }
 
